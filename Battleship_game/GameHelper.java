ed2029d9eb99c47adb40f928fb2a3cef5e63284a
ub <mudrovanddrey@gmail.com>
2023-05-25 19:17:59 +0300

    ...

--- Battleship_game/GameHelper.java
+++ Battleship_game/GameHelper.java
@@ -1,7 +1,75 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.ArrayList;
 
 public class GameHelper {
-    public ArrayList<String> getPlaceShip(int shipSize) {
+    private static final String alphabet = "abcdefg";
+    private int gridSize = 49;
+    private int shipCount = 0;
+    private int gridLength = 7;
+    private int[] grid = new int[gridSize];
 
+    public String getUserInput(String prompt) {
+        String line = null;
+        System.out.print(prompt + " ");
+        try {
+            BufferedReader is = new BufferedReader(new InputStreamReader(System.in));
+            line = is.readLine();
+            if (line.length() == 0) return null;
+        } catch (IOException e) {
+            System.out.println("IOException: " + e);
+        }
+        return line.toLowerCase();
+    }
+
+    public ArrayList<String> getPlaceShip(int shipSize) {
+        boolean success = false;
+        int attempts = 0;
+        int location = 0;
+        int[] coords = new int[shipSize];
+        ArrayList<String> alphaCells = new ArrayList<String>();
+        String[] alphaCoords = new String[shipSize];
+        String temp = null;
+
+        shipCount++;
+        int incrementToX = 1;
+        if ((shipCount % 2) == 1) {
+            incrementToX = gridLength;
+        }
+
+        while (!success & attempts++ < 200) {
+            location = (int)(Math.random() * gridSize);
+            int x = 0;
+            success = true;
+            while (success && x < shipSize) {
+                if (grid[location] == 0) {
+                    coords[x++] = location;
+                    location += incrementToX;
+                    if (location >= gridSize) {
+                        success = false;
+                    }
+                    if (x > 0 && (location % gridLength) == 0) {
+                        success = false;
+                    }
+                } else {
+                    success = false;
+                }
+            }
+        }
+
+        int x = 0, row = 0, col = 0;
+
+        while (x < shipSize) {
+            grid[coords[x]] = 1;
+            row = (int)(coords[x] / gridLength);
+            col = coords[x] % gridLength;
+            temp = String.valueOf(alphabet.charAt(col));
+
+            alphaCells.add(temp.concat(Integer.toString(row)));
+            x++;
+        }
+
+        return alphaCells;
     }
 }
--- Battleship_game/ShipBust.java
+++ Battleship_game/ShipBust.java
@@ -8,6 +8,7 @@ public class ShipBust {
     public static void main(String[] args)  {
         ShipBust battleShip = new ShipBust();
         battleShip.setUpGame();
+        battleShip.startPlaying();
     }
 
     private void setUpGame() {
@@ -28,14 +29,35 @@ public class ShipBust {
     }
 
     private void startPlaying() {
-
+        while (!shipList.isEmpty()) {
+            String userGuess = helper.getUserInput("Make a move");
+            checkUserGuess(userGuess);
+        }
+        finish();
     }
 
-    private void checkUserGuess() {
-
+    private void checkUserGuess(String userGuess) {
+        numOfGuess++;
+        String result = "past";
+        for (Ship ship : shipList) {
+            result = ship.checkYourSelf(userGuess);
+            if (result.equals("hit")) {
+                break;
+            }
+            if (result.equals("bust")) {
+                shipList.remove(ship);
+                break;
+            }
+        }
+        System.out.println(result);
     }
 
     private void finish() {
-
+        System.out.println("End of the game");
+        if (numOfGuess <= 18) {
+            System.out.println("It took you: " + numOfGuess + " attemp");
+        } else {
+            System.out.println("It took you a long time: " + numOfGuess + " ...");
+        }
     }
 }
