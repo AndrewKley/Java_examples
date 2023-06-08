fc778a03aa28206aff8fd055ea6b5c50a9d601b6
ub <mudrovanddrey@gmail.com>
2023-06-08 07:45:52 +0300

    ...

--- testWeb/src/main/java/com/example/controllers/MainController.java
+++ testWeb/src/main/java/com/example/controllers/MainController.java
@@ -2,14 +2,22 @@ package com.example.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class MainController {
-    @RequestMapping("/")
-    public String home(@RequestParam(required = false) String name, @RequestParam(required = false) String color, Model page) {
-        page.addAttribute("username", name);
+//    @RequestMapping("/")
+//    public String home(@RequestParam(required = false) String name, @RequestParam(required = false) String color, Model page) {
+//        page.addAttribute("username", name);
+//        page.addAttribute("color", color);
+//        return "home";
+//    }
+
+    @RequestMapping("/{color}")
+    public String home(@PathVariable String color, Model page) {
+        page.addAttribute("username", "Arkadiy");
         page.addAttribute("color", color);
         return "home";
     }
---
+++ testWeb/src/main/java/com/example/services/Productservice.java
@@ -0,0 +1,20 @@
+package com.example.services;
+
+import com.example.models.Product;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+public class Productservice {
+    private List<Product> products = new ArrayList<Product>();
+
+    public void addProduct(Product product) {
+        products.add(product);
+    }
+
+    public List<Product> findAll() {
+        return products;
+    }
+}
