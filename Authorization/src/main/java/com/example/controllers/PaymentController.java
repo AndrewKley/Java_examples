2fedaace7f79f0367cf29f7e9b2f7d8a658f2fc1
ub <mudrovanddrey@gmail.com>
2023-06-11 15:38:11 +0300

    data from client to server using HTTP

--- Authorization/src/main/java/com/example/controllers/PaymentController.java
+++ Authorization/src/main/java/com/example/controllers/PaymentController.java
@@ -6,8 +6,9 @@ import com.example.models.PaymentDetails;
 import com.example.services.PaymentService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.logging.Logger;
 
 @RestController
 public class PaymentController {
@@ -37,6 +38,18 @@ public class PaymentController {
     @GetMapping("/payment")
     public ResponseEntity<?> makePayment() {
         PaymentDetails paymentDetails = paymentService.processPayment();
+        return ResponseEntity
+                .status(HttpStatus.ACCEPTED)
+                .body(paymentDetails);
+    }
+
+    private static Logger logger = Logger.getLogger(PaymentController.class.getName());
+    // curl -v POST http://localhost:8080/payment -d '{"amount": 1001}' -H "Content-Type: application/json"
+    @PostMapping("/payment")
+    public ResponseEntity<PaymentDetails> makePaymentPost(
+            @RequestBody PaymentDetails paymentDetails
+    ) {
+        logger.info("Received payment " + paymentDetails.getAmount());
         return ResponseEntity
                 .status(HttpStatus.ACCEPTED)
                 .body(paymentDetails);
