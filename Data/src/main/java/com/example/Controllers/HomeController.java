57e8fbb0a3ab4169080b2a5a8c46df0cf5273c61
ub <mudrovanddrey@gmail.com>
2023-06-13 15:57:40 +0300

    adding a client

--- Data/src/main/java/com/example/Controllers/HomeController.java
+++ Data/src/main/java/com/example/Controllers/HomeController.java
@@ -1,27 +1,42 @@
 package com.example.Controllers;
 
-import com.example.Services.ClientService;
+import com.example.Repositories.ClientRepository;
 import com.example.models.Client;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-
-import java.util.List;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class HomeController {
-    private final ClientService clientService;
+    private final ClientRepository clientRepository;
 
     @Autowired
-    public HomeController(ClientService clientService) {
-        this.clientService = clientService;
+    public HomeController(ClientRepository clientRepository) {
+        this.clientRepository = clientRepository;
     }
 
 
     @GetMapping("/")
     public String home(Model model) {
-        List<Client> clients = clientService.getAllClient();
+        Iterable<Client> clients = clientRepository.findAll();
+        model.addAttribute("clients", clients);
+        return "home";
+    }
+
+    @PostMapping("/")
+    public String homeAddClient(
+            @RequestParam String login,
+            @RequestParam String password,
+            Model model
+    ) {
+        Client client = new Client();
+        client.setLogin(login);
+        client.setPassword(password);
+        clientRepository.save(client);
+        Iterable<Client> clients = clientRepository.findAll();
         model.addAttribute("clients", clients);
         return "home";
     }
